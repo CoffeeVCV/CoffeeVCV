f2978f1557669fa4f3efbc726c3b02c816f014f9
CoffeeVCV <104711251+CoffeeVCV@users.noreply.github.com>
2022-05-24 22:46:37 +1000

    Update Together.cpp
    Variable renaming.
    Clock divider range now 0 - 64
    Nudge trigger labels change from CV to Trig
    Renamed Step to Div
    Fixed randomize bug

--- src/Together.cpp
+++ src/Together.cpp
@@ -2,6 +2,12 @@
 #include "components.hpp"
 #define NUM_ROWS 8
 #define NUM_GROUPS 4
+#define NUM_DIVS 64.f
+
+// TODO 
+// Add feature to store and restore the settings of a row
+//    This will allow nudge to be used, and then return to a know pattern
+// 
 
 struct Together : Module {
 	enum ParamId {
@@ -9,15 +15,15 @@ struct Together : Module {
 		P_RESET_BUTTON,
 		P_NUDGE_BUTTON,
 		P_NUDGEV,
-		P_NudgeMode,
-		ENUMS(P_STEPS,NUM_ROWS),
+		P_nudgeMode,
+		ENUMS(P_DIVS,NUM_ROWS),
 		ENUMS(P_G1,NUM_ROWS),
 		ENUMS(P_G2,NUM_ROWS),
 		ENUMS(P_G3,NUM_ROWS),
 		ENUMS(P_G4,NUM_ROWS),
 		ENUMS(P_GNUDGE_BUTTON,NUM_GROUPS),
 		ENUMS(P_RNUDGE_BUTTON,NUM_ROWS),	
-		ENUMS(P_STEPLOCK,NUM_ROWS),	
+		ENUMS(P_DIVLOCK,NUM_ROWS),	
 		ENUMS(P_RTRIGPROB,NUM_ROWS),	
 		P_SNUDGE_BUTTON,
 		P_SCALE,
@@ -28,9 +34,9 @@ struct Together : Module {
 		I_TRIG,
 		I_RESET,
 		I_NUDGE,
-		I_StepCV,
-		ENUMS(I_GCV,NUM_GROUPS),
-		ENUMS(I_RCV,NUM_ROWS),
+		I_DIVTRIG,
+		ENUMS(I_GROUPNUDGETRIG,NUM_GROUPS),
+		ENUMS(I_RowNudgeTrig,NUM_ROWS),
 		INPUTS_LEN
 	};
 	enum OutputId {
@@ -52,7 +58,7 @@ struct Together : Module {
 		LIGHTS_LEN
 	};
 
-	enum NudgeMode {
+	enum nudgeMode {
 		NUDGE,
 		RANDOM
 	};
@@ -61,21 +67,21 @@ struct Together : Module {
 	dsp::BooleanTrigger triggerButton;
 	dsp::BooleanTrigger grouprndButton[NUM_GROUPS];
 	dsp::BooleanTrigger rowrndButton[NUM_ROWS];
-	dsp::BooleanTrigger steprndButton;
-	dsp::BooleanTrigger stepLockButton[NUM_ROWS];
+	dsp::BooleanTrigger divNudgeButton;
+	dsp::BooleanTrigger divLockButton[NUM_ROWS];
 	dsp::BooleanTrigger nudgeButton;
 	dsp::BooleanTrigger nudgemodeButton;
 	
 	dsp::SchmittTrigger nudgeTrigger;
 	dsp::SchmittTrigger clockTrigger;
 	dsp::SchmittTrigger resetTrigger;
-	dsp::SchmittTrigger grouprndTrigger[NUM_GROUPS];
-	dsp::SchmittTrigger rowrndTrigger[NUM_ROWS];
+	dsp::SchmittTrigger groupNudgeTrigger[NUM_GROUPS];
+	dsp::SchmittTrigger rowNudgeTrigger[NUM_ROWS];
 
 	dsp::PulseGenerator orPulse;
 	dsp::PulseGenerator rowPulse[NUM_ROWS];
 
-	dsp::ClockDivider LowPriority;
+	dsp::ClockDivider lowPriority;
 
 	long seqCount=0;
 	bool cycleResult[NUM_ROWS]={true,true,true,true,true,true,true,true};
@@ -83,8 +89,8 @@ struct Together : Module {
 	bool pulse;
 	float range=1.f;
 	float nudgev=0.05f;
-	bool modetoggleready=true;
-	int NudgeMode = RANDOM;
+	bool modeToggleReady=true;
+	int nudgeMode = RANDOM;
 
 	Together() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
@@ -92,8 +98,8 @@ struct Together : Module {
 		configButton(P_RESET_BUTTON, "Reset");
 		// each row's 
 		for(int i=0; i<NUM_ROWS; i++){
-			configParam(P_STEPS + i, 0.f, 16.f, 0.f,  string::f("Steps %d", i + 1) );
-			paramQuantities[P_STEPS + i]->snapEnabled = true;
+			configParam(P_DIVS + i, 0.f, NUM_DIVS, 0.f,  string::f("Steps %d", i + 1) );
+			paramQuantities[P_DIVS + i]->snapEnabled = true;
 			configParam(P_RTRIGPROB + i, 0.f, 1.f, 1.f, string::f("Row %d Trig Probability", i + 1));
 			configParam(P_G1 + i, -range, range, 0.f, string::f("Group 1 Row %d", i + 1));
 			configParam(P_G2 + i, -range, range, 0.f, string::f("Group 2 Row %d", i + 1));
@@ -101,45 +107,46 @@ struct Together : Module {
 			configParam(P_G4 + i, -range, range, 0.f, string::f("Group 2 Row %d", i + 1));
 			configOutput(O_RTRIG + i, string::f("Row %d Trig", i + 1));
 			configOutput(O_RGATE + i, string::f("Row %d Gate", i + 1));
-			configButton(P_RNUDGE_BUTTON + i,string::f("Row %d Randomize", i + 1));
-			configButton(P_STEPLOCK + i,string::f("Row %d Step Lock", i + 1));
-			configInput(I_RCV + i, string::f("Row %d CV", i + 1));
+			configButton(P_RNUDGE_BUTTON + i,string::f("Row %d Rand", i + 1));
+			configButton(P_DIVLOCK + i,string::f("Row %d Divider Lock", i + 1));
+			configInput(I_RowNudgeTrig + i, string::f("Row %d Rand Trig", i + 1));
 		}
 
-		configInput(I_NUDGE, "CV Nudge");
+		configInput(I_NUDGE, "Nudge Trig");
 		configButton(P_NUDGE_BUTTON,"Manual Nudge");
 		configParam(P_NUDGEV, 0.f, 1.f, 0.05f, "Nudge Amount");
-		configButton(P_NudgeMode,"Nudge/RND Mode");
+		configButton(P_nudgeMode,"Nudge/RND Mode");
 
 		//scale and offset
 		configParam(P_SCALE, -5, 5, 1.f, "Scale");
 		configParam(P_OFFSET, -5, 5, 0.f, "Offset");
 		// the groups
 		for(int i=0;i<NUM_GROUPS;i++){
-			configInput(I_GCV + i,string::f("Grp CV %d", i + 1));
+			configInput(I_GROUPNUDGETRIG + i,string::f("Grp %d Rand Trig", i + 1));
 			configOutput(O_G + i, string::f("Out Grp %d", i + 1));
-			configButton(P_GNUDGE_BUTTON + i,string::f("Group %d Randomize", i + 1));
+			configButton(P_GNUDGE_BUTTON + i,string::f("Group %d Rand", i + 1));
 		}
-		configButton(P_SNUDGE_BUTTON,"Randomize Steps");
-		configInput(I_StepCV, "Step CV");
+		configButton(P_SNUDGE_BUTTON,"Randomize Dividers");
+		configInput(I_DIVTRIG, "Step CV");
 		configInput(I_TRIG, "Trig");
 		configInput(I_RESET, "Reset");
 		configOutput(O_ORTRIG, "Global out trig");
 		configOutput(O_ORGATE, "Global out gate");
 
-		LowPriority.division=4;
+		lowPriority.division=4;
 		lights[L_NUDGEMODE].setBrightness(0);
 		lights[L_RNDMODE].setBrightness(1);
 	}
 
-	float nudge(float pv, float rndammount, float nudgeammount, float lo, float hi, int mode) {
-		float nv;
-		if(NudgeMode==NUDGE) {
-			float v=(random::uniform()>=0.5) ? pv - nudgeammount : pv + nudgeammount;
-			nv=clamp(v,lo,hi);
+	float nudge(float currentValue, float rndAmmount, float nudgeAmmount, float lo, float hi, int mode) {
+		float newValue;
+		if(nudgeMode==NUDGE) {
+			float v=(random::uniform()>0.5) ? currentValue - nudgeAmmount : currentValue + nudgeAmmount;
+			newValue=clamp(v,lo,hi);
 		} else {
-			nv= ((rndammount*2)*random::uniform())-rndammount;		}
-		return nv;
+			newValue= ((hi-lo)*random::uniform())+lo;
+		}
+		return newValue;
 	}
 
 	void process(const ProcessArgs& args) override {
@@ -159,14 +166,14 @@ struct Together : Module {
 		}
 		
 		//lowpriority
-		if(LowPriority.process()){
+		if(lowPriority.process()){
 			//nudge or rnd the steps
-			if(steprndButton.process(params[P_SNUDGE_BUTTON].getValue())){
+			if(divNudgeButton.process(params[P_SNUDGE_BUTTON].getValue())){
 				for(int row=0; row<NUM_ROWS; row++){
 					//not the locked steps
-					if(!params[P_STEPLOCK + row].getValue()) {
-						float v=params[P_STEPS + row].getValue();
-						params[P_STEPS + row].setValue(nudge(v,16,1,0,16,NudgeMode));
+					if(!params[P_DIVLOCK + row].getValue()) {
+						float currentValue=params[P_DIVS + row].getValue();
+						params[P_DIVS + row].setValue(int(nudge(currentValue,16,1,0,16,nudgeMode)));
 					}
 				}
 			}
@@ -175,49 +182,49 @@ struct Together : Module {
 			nudgev=params[P_NUDGEV].getValue();
 			for(int i=0; i<NUM_ROWS;i++){
 				float button=rowrndButton[i].process(params[P_RNUDGE_BUTTON +i].getValue());
-				float trigger=rowrndTrigger[i].process(inputs[I_RCV + i].getVoltage(), 0.1f, 1.f);
+				float trigger=rowNudgeTrigger[i].process(inputs[I_RowNudgeTrig + i].getVoltage(), 0.1f, 1.f);
 				if(button || trigger){
-					params[P_G1 + i].setValue( nudge(params[P_G1 + i].getValue(),range,nudgev,-range,range,NudgeMode ) );
-					params[P_G2 + i].setValue( nudge(params[P_G2 + i].getValue(),range,nudgev,-range,range,NudgeMode ) );
-					params[P_G3 + i].setValue( nudge(params[P_G3 + i].getValue(),range,nudgev,-range,range,NudgeMode ) );
-					params[P_G4 + i].setValue( nudge(params[P_G4 + i].getValue(),range,nudgev,-range,range,NudgeMode ) );			
+					params[P_G1 + i].setValue( nudge(params[P_G1 + i].getValue(),range,nudgev,-range,range,nudgeMode ) );
+					params[P_G2 + i].setValue( nudge(params[P_G2 + i].getValue(),range,nudgev,-range,range,nudgeMode ) );
+					params[P_G3 + i].setValue( nudge(params[P_G3 + i].getValue(),range,nudgev,-range,range,nudgeMode ) );
+					params[P_G4 + i].setValue( nudge(params[P_G4 + i].getValue(),range,nudgev,-range,range,nudgeMode ) );			
 				}	
 			}
 
 			//randomize column
 			for(int i=0; i<NUM_GROUPS;i++){
 				float button=grouprndButton[i].process(params[P_GNUDGE_BUTTON +i].getValue());
-				float trigger=grouprndTrigger[i].process(inputs[I_GCV + i].getVoltage(), 0.1f, 1.f);
+				float trigger=groupNudgeTrigger[i].process(inputs[I_GROUPNUDGETRIG + i].getVoltage(), 0.1f, 1.f);
 				if( button || trigger ) {
 					for(int row=0; row<NUM_ROWS; row++){
-						if(i==0) params[P_G1 + row].setValue(nudge(params[P_G1 + row].getValue(),range,nudgev,-range,range ,NudgeMode) );
-						if(i==1) params[P_G2 + row].setValue(nudge(params[P_G2 + row].getValue(),range,nudgev,-range,range ,NudgeMode) );
-						if(i==2) params[P_G3 + row].setValue(nudge(params[P_G3 + row].getValue(),range,nudgev,-range,range ,NudgeMode) );
-						if(i==3) params[P_G4 + row].setValue(nudge(params[P_G4 + row].getValue(),range,nudgev,-range,range ,NudgeMode) );					
+						if(i==0) params[P_G1 + row].setValue(nudge(params[P_G1 + row].getValue(),range,nudgev,-range,range ,nudgeMode) );
+						if(i==1) params[P_G2 + row].setValue(nudge(params[P_G2 + row].getValue(),range,nudgev,-range,range ,nudgeMode) );
+						if(i==2) params[P_G3 + row].setValue(nudge(params[P_G3 + row].getValue(),range,nudgev,-range,range ,nudgeMode) );
+						if(i==3) params[P_G4 + row].setValue(nudge(params[P_G4 + row].getValue(),range,nudgev,-range,range ,nudgeMode) );					
 					}
 				}
 			}
 
 			//step lock
 			for(int row=0; row<NUM_ROWS; row++){
-				lights[L_STEPLOCK + row].setBrightness(params[P_STEPLOCK + row].getValue());
+				lights[L_STEPLOCK + row].setBrightness(params[P_DIVLOCK + row].getValue());
 			}
 
 
 			// Mode toggle
-			if ( modetoggleready && nudgemodeButton.process(params[P_NudgeMode].getValue())) {
-				if(NudgeMode==NUDGE) {
-					NudgeMode=RANDOM;
+			if ( modeToggleReady && nudgemodeButton.process(params[P_nudgeMode].getValue())) {
+				if(nudgeMode==NUDGE) {
+					nudgeMode=RANDOM;
 					lights[L_NUDGEMODE].setBrightness(0);
 					lights[L_RNDMODE].setBrightness(1);
 				} else {
-					NudgeMode=NUDGE;
+					nudgeMode=NUDGE;
 					lights[L_RNDMODE].setBrightness(0);
 					lights[L_NUDGEMODE].setBrightness(1);
 				}
-				modetoggleready=false;
+				modeToggleReady=false;
 			} else {
-				modetoggleready=true;
+				modeToggleReady=true;
 			}
 
 			// nudge
@@ -225,10 +232,10 @@ struct Together : Module {
 				nudgev=params[P_NUDGEV].getValue();
 				for(int i=0; i<NUM_GROUPS;i++){
 					for(int row=0; row<NUM_ROWS; row++){
-						if(i==0) params[P_G1 + row].setValue(nudge( params[P_G1 + row].getValue(),range,nudgev,-range,range ,NudgeMode));
-						if(i==1) params[P_G2 + row].setValue(nudge( params[P_G2 + row].getValue(),range,nudgev,-range,range ,NudgeMode));
-						if(i==2) params[P_G3 + row].setValue(nudge( params[P_G3 + row].getValue(),range,nudgev,-range,range ,NudgeMode));
-						if(i==3) params[P_G4 + row].setValue(nudge( params[P_G4 + row].getValue(),range,nudgev,-range,range ,NudgeMode));					
+						if(i==0) params[P_G1 + row].setValue(nudge( params[P_G1 + row].getValue(),range,nudgev,-range,range ,nudgeMode));
+						if(i==1) params[P_G2 + row].setValue(nudge( params[P_G2 + row].getValue(),range,nudgev,-range,range ,nudgeMode));
+						if(i==2) params[P_G3 + row].setValue(nudge( params[P_G3 + row].getValue(),range,nudgev,-range,range ,nudgeMode));
+						if(i==3) params[P_G4 + row].setValue(nudge( params[P_G4 + row].getValue(),range,nudgev,-range,range ,nudgeMode));					
 					}
 				}
 			}
@@ -247,7 +254,7 @@ struct Together : Module {
 
 				//check if row is triggered based on steps
 				//just note which rows will trigger, do it later
-				int v=(int)params[P_STEPS + i].getValue();
+				int v=(int)params[P_DIVS + i].getValue();
 				if(v==0) {  
 			 		cycleResult[i] = false;
 					lights[L_STEPACTIVE + i].setBrightness(0);
@@ -267,9 +274,10 @@ struct Together : Module {
 			for(int row=0; row<NUM_ROWS; row++){
 				if(cycleResult[row]) {	
 					//triggers and gates
+					anytrig=true;
+
 					//check prob
 					if(random::uniform() <= params[P_RTRIGPROB + row].getValue() ){
-						anytrig=true;
 						rowPulse[row].trigger(1e-3f);
 						outputs[O_RGATE + row].setVoltage(10.f);
 						lights[L_TRIGHEADS +row].setBrightness(1);
@@ -332,8 +340,8 @@ struct TogetherWidget : ModuleWidget {
 		for(int i=0; i<NUM_ROWS; i++){
 			x=lx;
 			y=yoff+(i*s);
-			addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(x, y)), module, Together::P_STEPS + i));
-			addParam(createParamCentered<CoffeeTinyButtonLatch>(mm2px(Vec(x-4, y-4)), module, Together::P_STEPLOCK + i ));
+			addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(x, y)), module, Together::P_DIVS + i));
+			addParam(createParamCentered<CoffeeTinyButtonLatch>(mm2px(Vec(x-4, y-4)), module, Together::P_DIVLOCK + i ));
 			addChild(createLightCentered<CoffeeTinySimpleLight<OrangeLight>>(mm2px(Vec(x-4, y-4)), module, Together::L_STEPLOCK + i));
 			x=5+lx+(s*1);
 			addParam(createParamCentered<Trimpot>(mm2px(Vec(x, y)), module, Together::P_G1 + i));
@@ -345,7 +353,7 @@ struct TogetherWidget : ModuleWidget {
 			addParam(createParamCentered<Trimpot>(mm2px(Vec(x, y)), module, Together::P_G4 + i));
 
 			x=5+lx+(s*5);
-			addInput(createInputCentered<CoffeeInputPortButton>(mm2px(Vec(x, y)), module, Together::I_RCV + i));
+			addInput(createInputCentered<CoffeeInputPortButton>(mm2px(Vec(x, y)), module, Together::I_RowNudgeTrig + i));
 			addParam(createParamCentered<CoffeeTinyButton>(mm2px(Vec(x+3.5, y-3.5)), module, Together::P_RNUDGE_BUTTON + i ));
 
 			x=5+lx+(s*6);
@@ -367,13 +375,13 @@ struct TogetherWidget : ModuleWidget {
 		//Step CV input
 		y=16;
 		x=lx;
-		addInput(createInputCentered<CoffeeInputPortButton>(mm2px(Vec(x, y)), module, Together::I_StepCV));
+		addInput(createInputCentered<CoffeeInputPortButton>(mm2px(Vec(x, y)), module, Together::I_DIVTRIG));
 		addParam(createParamCentered<CoffeeTinyButton>(mm2px(Vec(x+3.5, y-3.5)), module, Together::P_SNUDGE_BUTTON));
 
 		//nudge vs rnd switch
 		y=16;
 		x=lx+7.5;
-		addParam(createParamCentered<CoffeeButtonVertIndicator>(mm2px(Vec(x, y)), module, Together::P_NudgeMode));
+		addParam(createParamCentered<CoffeeButtonVertIndicator>(mm2px(Vec(x, y)), module, Together::P_nudgeMode));
 		addChild(createLightCentered<TinyLight<OrangeLight>>(mm2px(Vec(x, y)), module, Together::L_NUDGEMODE));
 		addChild(createLightCentered<TinyLight<OrangeLight>>(mm2px(Vec(x, y+2)), module, Together::L_RNDMODE));
 
@@ -382,7 +390,7 @@ struct TogetherWidget : ModuleWidget {
 		for(int i=0; i<NUM_GROUPS; i++){
 			y=16;
 			x=lx+(s*i)+s+5;
-			addInput(createInputCentered<CoffeeInputPortButton>(mm2px(Vec(x, y)), module, Together::I_GCV + i));
+			addInput(createInputCentered<CoffeeInputPortButton>(mm2px(Vec(x, y)), module, Together::I_GROUPNUDGETRIG + i));
 			addParam(createParamCentered<CoffeeTinyButton>(mm2px(Vec(x+3.5, y-3.5)), module, Together::P_GNUDGE_BUTTON + i ));
 		}
 
