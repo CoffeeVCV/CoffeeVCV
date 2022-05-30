ef08ca0af334f59cc1d652ef90be84eb488bad0d
CoffeeVCV <104711251+CoffeeVCV@users.noreply.github.com>
2022-05-30 20:36:45 +1000

    Update to Travel
        * Cosmetic changes
        * Move option from context menu to front panel
        * Changed input calculation to source+offset*scale

--- src/Travel.cpp
+++ src/Travel.cpp
@@ -1,4 +1,5 @@
 #include "plugin.hpp"
+#include "components.hpp"
 
 struct Travel : Module {
 	enum ParamId {
@@ -10,6 +11,7 @@ struct Travel : Module {
 		P_SCALE2,
 		P_OFFSET2,
 		P_SHAPE,
+		P_TRACKHOLD,
 		PARAMS_LEN
 	};
 	enum InputId {
@@ -27,6 +29,7 @@ struct Travel : Module {
 	};
 	enum LightId {
 		L_PROCESS,
+		L_ACTIVE,
 		LIGHTS_LEN
 	};
 
@@ -42,16 +45,16 @@ struct Travel : Module {
 	float i_CV1=0.f;
 	float p_shape=0.f;
 	float targetDuration=0.f;
-	bool track=false;
 
 	Travel() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		configButton(P_TRIG1, "Manual Trigger");
 		configSwitch(P_DURATIONSCALE, 0, 2, 0, "Duration scale", {"1", "10", "100"});
+		configSwitch(P_TRACKHOLD, 0, 1, 0, "Track or Hold", {"Hold", "Track"});
 		configParam(P_DURATION, 0.f, 1.f, 1.f, "Duration");
 		configParam(P_OFFSET1, -5.0f, 5.0f, 0.0f, "Offset", " V");
-		configParam(P_SCALE1, 0.0f, 1.0f, 1.0f, "Scale", "%", 0.0f, 100.0f);
-		configParam(P_SCALE2, 0.0f, 1.0f, 1.0f, "Scale", "%", 0.0f, 100.0f);
+		configParam(P_SCALE1, -5.0f, 5.0f, 1.0f, "Scale");
+		configParam(P_SCALE2, -5.0f, 5.0f, 1.0f, "Scale");
 		configParam(P_OFFSET2, -5.0f, 5.0f, 0.0f, "Offset", " V");
 		configParam(P_SHAPE, -5.0f, 5.0f, 0.0f, "Shape");
 		configInput(I_TRIG, "Trigger");
@@ -71,8 +74,8 @@ struct Travel : Module {
 		float p_offset2=params[P_OFFSET2].getValue();
 		int p_durationscale=params[P_DURATIONSCALE].getValue();
 		//get inputs
-		i_CV1=(inputs[I_CV1].getVoltage() * p_scale1) + p_offset1;
-		i_CV2=(inputs[I_CV2].getVoltage() * p_scale2) + p_offset2;
+		i_CV1=(inputs[I_CV1].getVoltage() + p_offset1) * p_scale1;
+		i_CV2=(inputs[I_CV2].getVoltage() + p_offset2) * p_scale2;
 
 		//get duration
 		targetDuration=params[P_DURATION].getValue() * durationScales[p_durationscale];
@@ -95,18 +98,15 @@ struct Travel : Module {
 		// start the cycle
 		float b_trigger=params[P_TRIG1].getValue();
 		float i_trigger=inputs[I_TRIG].getVoltage();
-		if( (!cycling | track) && (clockTrigger.process(i_trigger) | buttonTrigger.process(b_trigger))){
+		bool track=(params[P_TRACKHOLD].getValue()==0) ? false : true;
+		if( (!cycling || track) && (clockTrigger.process(i_trigger) || buttonTrigger.process(b_trigger))){
 			cycleStart=args.frame;
 			cycling=true;
 		} 
 
-		if((cycling && track) | !cycling ){
+		if((cycling && track) || !cycling ){
 			updateInputs(args);
 		}
-
-
-
-
 
 		if(cycling){
 			cycleProgress=((args.frame-cycleStart)/args.sampleRate)/targetDuration;
@@ -140,44 +140,38 @@ struct Travel : Module {
 struct TravelWidget : ModuleWidget {
 	TravelWidget(Travel* module) {
 		setModule(module);
-		float lx=30.48/4;
-		float rx=30.48-lx;
-		float mx=30.48/2;
+		float lx = 20.32 / 4;
+		float rx = lx + 10;
+		float s = 10;
+		float yOffset=15;
+		float y=yOffset;
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/Travel.svg")));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(lx, 15)), module, Travel::I_TRIG));
-		addParam(createParamCentered<VCVButton>(mm2px(Vec(rx, 15)), module, Travel::P_TRIG1));
+		addInput(createInputCentered<CoffeeInputPortButton>(mm2px(Vec(lx, y)), module, Travel::I_TRIG));
+		addParam(createParamCentered<CoffeeTinyButton>(mm2px(Vec(lx+3.5, y-3.5)), module, Travel::P_TRIG1));
+		addChild(createLightCentered<MediumLight<OrangeLight>>(mm2px(Vec(lx, y+s)), module, Travel::L_ACTIVE));
+		addParam(createParamCentered<CoffeeSwitch2PosHori>(mm2px(Vec(rx, y)), module, Travel::P_TRACKHOLD));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(lx, 30)), module, Travel::I_DURATION));
-		addParam(createParamCentered<Trimpot>(mm2px(Vec(rx, 30)), module, Travel::P_DURATION));
-		addParam(createParamCentered<CKSSThreeHorizontal>(mm2px(Vec(mx, 37.5)), module, Travel::P_DURATIONSCALE));
-		addChild(createLightCentered<MediumLight<GreenLight>>(mm2px(Vec(mx, 30)), module, Travel::L_PROCESS));
 
-		addParam(createParamCentered<Trimpot>(mm2px(Vec(rx, 50)), module, Travel::P_SHAPE));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(lx, 50)), module, Travel::I_SHAPE));
+		y=35;
+		addInput(createInputCentered<CoffeeInputPort>(mm2px(Vec(lx, y)), module, Travel::I_DURATION));
+		addParam(createParamCentered<CoffeeKnob6mm>(mm2px(Vec(rx, y)), module, Travel::P_DURATION));
+		addParam(createParamCentered<CoffeeSwitch3PosHori>(mm2px(Vec(rx, y+s)), module, Travel::P_DURATIONSCALE));
+		addChild(createLightCentered<MediumLight<OrangeLight>>(mm2px(Vec(lx, y+s)), module, Travel::L_PROCESS));
 
-		float y=70;
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(lx, y)), module, Travel::I_CV1));
-		addParam(createParamCentered<Trimpot>(mm2px(Vec(lx, y+10)), module, Travel::P_SCALE1));
-		addParam(createParamCentered<Trimpot>(mm2px(Vec(lx, y+20)), module, Travel::P_OFFSET1));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(rx, y)), module, Travel::I_CV2));
-		addParam(createParamCentered<Trimpot>(mm2px(Vec(rx, y+10)), module, Travel::P_SCALE2));
-		addParam(createParamCentered<Trimpot>(mm2px(Vec(rx, y+20)), module, Travel::P_OFFSET2));
+		addParam(createParamCentered<CoffeeKnob6mm>(mm2px(Vec(rx, y+s+s+5)), module, Travel::P_SHAPE));
+		addInput(createInputCentered<CoffeeInputPort>(mm2px(Vec(lx, y+s+s+5)), module, Travel::I_SHAPE));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(lx, 112)), module, Travel::O_CV1));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(rx, 112)), module, Travel::O_EOC));
-	}
-	void appendContextMenu(Menu* menu) override {
-			Travel* module = dynamic_cast<Travel*>(this->module);
-			assert(module);
+		y=yOffset+(s*6);
+		addInput(createInputCentered<CoffeeInputPort>(mm2px(Vec(lx, y)), module, Travel::I_CV1));
+		addParam(createParamCentered<CoffeeKnob6mm>(mm2px(Vec(lx, y+10)), module, Travel::P_SCALE1));
+		addParam(createParamCentered<CoffeeKnob6mm>(mm2px(Vec(lx, y+20)), module, Travel::P_OFFSET1));
+		addInput(createInputCentered<CoffeeInputPort>(mm2px(Vec(rx, y)), module, Travel::I_CV2));
+		addParam(createParamCentered<CoffeeKnob6mm>(mm2px(Vec(rx, y+10)), module, Travel::P_SCALE2));
+		addParam(createParamCentered<CoffeeKnob6mm>(mm2px(Vec(rx, y+20)), module, Travel::P_OFFSET2));
 
-			menu->addChild(new MenuSeparator());
-			menu->addChild(createSubmenuItem("Track/Hold Inputs", "", [=](Menu* menu) {
-				Menu* trackholdMenu = new Menu();
-				trackholdMenu->addChild(createMenuItem("Track", CHECKMARK(module->track == true), [module]() { module->track = true; }));
-				trackholdMenu->addChild(createMenuItem("Hold", CHECKMARK(module->track == false), [module]() { module->track = false; }));
-				menu->addChild(trackholdMenu);
-			}));
-		}	
+		addOutput(createOutputCentered<CoffeeOutputPort>(mm2px(Vec(lx, 112)), module, Travel::O_CV1));
+		addOutput(createOutputCentered<CoffeeOutputPort>(mm2px(Vec(rx, 112)), module, Travel::O_EOC));
+	}	
 };
 
 
